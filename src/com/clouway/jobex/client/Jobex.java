12cf1f47bb4d8c5e18e383e33db9fdfd29dd939a
Adelin <webmaster1803@gmail.com>
2012-07-06 17:47:24 +0300

    First commit

---
+++ src/com/clouway/jobex/client/Jobex.java
@@ -0,0 +1,16 @@
+package com.clouway.jobex.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>
+ */
+public class Jobex implements EntryPoint {
+
+  /**
+   * This is the entry point method.
+   */
+  public void onModuleLoad() {
+
+  }
+}
